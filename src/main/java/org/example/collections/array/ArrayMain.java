3afd3fd6d7324600ad86e70a114890e4decfa66a
heeom <swimxdev@gmail.com>
2024-09-05 21:53:45 +0900

    feat : List 구현

---
+++ src/main/java/org/example/collections/array/ArrayMain.java
@@ -0,0 +1,8 @@
+package org.example.collections.array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+    }
+}
---
+++ src/main/java/org/example/collections/array/MyArrayListV1.java
@@ -0,0 +1,52 @@
+package org.example.collections.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV1 {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elems;
+    private int size = 0;
+
+    public MyArrayListV1() {
+        elems = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV1(int initialCapacity) {
+        elems = new Object[initialCapacity];
+        this.size = initialCapacity;
+    }
+
+    public void add(Object o) {
+        elems[size] = o;
+        size++;
+    }
+
+    public Object get(int index) {
+        return elems[index];
+    }
+
+    public Object set(int index, Object elem) {
+        Object oldValue = this.get(index);
+        elems[index] = elem;
+        return oldValue;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elems[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayListV1{" +
+                "elems=" + Arrays.toString(Arrays.copyOf(elems, size)) + // Arrays.copyOf(elems, size) : size 크기의 배열을 새로 만듬
+                ", size=" + size +
+                ", capacity=" + elems.length +
+                '}';
+    }
+}
