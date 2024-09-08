87240f7bc5ebddf2e565b07afe72cf424437ee8d
heeom <swimxdev@gmail.com>
2024-09-08 22:18:50 +0900

    feat : 노드 탐색 구현

--- src/main/java/org/example/collections/link/NodeMain.java
+++ src/main/java/org/example/collections/link/NodeMain.java
@@ -5,5 +5,12 @@ public class NodeMain {
     public static void main(String[] args) {
         Node first = new Node("A");
         first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        Node node = first;
+        while(node.next != null) {
+            System.out.println(node.item);
+            node = node.next;
+        }
     }
 }
