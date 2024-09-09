1a7e484278ccf95d41bfec0e4a9f625080c3e4a0
heeom <swimxdev@gmail.com>
2024-09-09 21:50:10 +0900

    feat : 특정 위치에 node 추가 기능

--- src/main/java/org/example/collections/link/MyLinkedList.java
+++ src/main/java/org/example/collections/link/MyLinkedList.java
@@ -16,9 +16,36 @@ public class MyLinkedList {
         size++;
     }
 
+    public void add(int index, Object o) {
+        Node newNode = new Node(o);
+        if (index == 0) {
+            newNode.next = root;
+        } else {
+            Node prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++;
+    }
+
     private Node getLastNode() {
         Node node = root;
         while (node.next != null) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public Object set(int idx, Object o) {
+        Node node = getNode(idx);
+        Object item = node.item;
+        node.item = o;
+        return item;
+    }
+
+    public Node getNode(int idx) {
+        Node node = root;
+        for (int i = 0; i < idx; i++) {
             node = node.next;
         }
         return node;
