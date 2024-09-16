0cde9c03735302b1fe4a34dc8d539588a60f78ca
heeom <swimxdev@gmail.com>
2024-09-16 17:41:42 +0900

    feat : SynchronizedList 예시

---
+++ src/main/java/org/example/collections/utils/SyncMain.java
@@ -0,0 +1,19 @@
+package org.example.collections.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SyncMain {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println("list class :" + list.getClass());
+        List<Integer> synchronizedList = Collections.synchronizedList(list);
+        System.out.println("synchronizedList class :" + synchronizedList.getClass());
+    }
+}
