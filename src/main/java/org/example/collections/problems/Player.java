62d0d3681ed4c4534b0dab4db3a461d5669ce4c1
heeom <swimxdev@gmail.com>
2024-09-21 18:27:06 +0900

    feat : Player 구현

---
+++ src/main/java/org/example/collections/problems/Player.java
@@ -0,0 +1,13 @@
+package org.example.collections.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private List<Card> cards;
+
+    public Player() {
+        this.cards = new ArrayList<>();
+    }
+}
