e1ef0b025ac8e5b0e95bdfb1689b09f91df82506
heeom <swimxdev@gmail.com>
2024-09-21 18:02:16 +0900

    임시커밋

--- src/main/java/org/example/collections/utils/EmptyListMain.java
+++ src/main/java/org/example/collections/utils/EmptyListMain.java
@@ -15,6 +15,7 @@ public class EmptyListMain {
 
         // empty 가변 리스트
         List<Integer> list3 = Collections.emptyList(); // java 5
+        list3.add(1);
         List<Integer> list4 = List.of(); // java 9
 
         System.out.println("list3 = " + list3.getClass());
@@ -23,9 +24,11 @@ public class EmptyListMain {
         // 완전 불변 리스트 아님
         // 고정된 크기를 가지지만, 요소 변경은 가능, set을 통해 변경은 가능하지만 add(), remove()로 길이변경은 불가능
         List<Integer> list5 = Arrays.asList(1, 2, 3); // java 1.2 : 배열 -> List
+        System.out.println("list5 = " + list5.getClass());
 
         Integer[] arr = {1,2,3,4,5};
         List<Integer> arr1 = List.of(arr); // 새로운 리스트를 생성
         List<Integer> arr2 = Arrays.asList(arr); // 리스트 생성시 배열의 참조값을 그대로 사용 -> 리스트 생성하는 비용이 더 적음 / 배열의 크기가 아주 크다면 Arrays.asList가 나을 수도 있음
+        System.out.println("arr2 = " + arr2.getClass());
     }
 }
