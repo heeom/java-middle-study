824019c4a855ca51bbda319507080a505c3be7a0
heeom <swimxdev@gmail.com>
2024-09-21 18:52:01 +0900

    feat : 카드 뽑는 메서드 구현

--- src/main/java/org/example/collections/problems/Deck.java
+++ src/main/java/org/example/collections/problems/Deck.java
@@ -1,18 +1,27 @@
 package org.example.collections.problems;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Deck {
 
-    List<Card> cards;
+    private List<Card> cards;
+    private int index = -1;
 
     public Deck() {
-        this.cards = new ArrayList<>();
+        List<Card> cards = new ArrayList<>();
         for (Suits suits : Suits.values()) {
             for (int i = 1; i < 14; i++) {
                 cards.add(new Card(suits, i));
             }
         }
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * 카드 뽑기
+     * @return card
+     */
+    public Card draw() {
+        return cards.get(++index);
     }
 }
