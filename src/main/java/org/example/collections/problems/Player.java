fb2ed6caf6154ce682aff956e4546745aa7f439e
heeom <swimxdev@gmail.com>
2024-09-21 18:43:57 +0900

    feat : play, sum 함수 구현

--- src/main/java/org/example/collections/problems/Player.java
+++ src/main/java/org/example/collections/problems/Player.java
@@ -10,4 +10,29 @@ public class Player {
     public Player() {
         this.cards = new ArrayList<>();
     }
+
+    /**
+     * positive -> win
+     * negative -> lose
+     * zero -> tie
+     * @param competitor
+     * @return
+     */
+    public int play(Player competitor) {
+        return this.sum() - competitor.sum();
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (Card card : cards) {
+            sum += card.getNumber();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return "카드: [" + cards +
+                "], 합계:" + this.sum();
+    }
 }
